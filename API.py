ad53ef5636985b82495636911ae58e904f26dc9b
Zoraz Haseeb <78059639+Zoraz01@users.noreply.github.com>
2022-05-23 11:02:44 -0400

    Add files via upload

--- API.py
+++ API.py
@@ -1,17 +1,20 @@
 import time
-
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import urllib.request
+# spotipy is required for this application to work
 
-cid = 'Enter your own client id here'
-secret = 'enter your own client id here'
+# client id and secret id that corresponds with your account can be acquired from Spotify API website
+cid = '924d5bddc07f4588a2bd13c7fef258f9'
+secret = 'ae858f86e4ae4ee489652037637a23c5'
 scope = "user-read-currently-playing"
+
 def get_current_song():
-    ##client_credentials_manager = SpotifyClientCredentials(client_id= cid,client_secret=secret)
+    # Spotify object is created, functions from spotipy can be used on this object to acquire new information
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,client_secret=secret,redirect_uri='http://example.com',scope=scope))
+    # All the information for the currently playing song is assigned to results
     results = sp.currently_playing(market='US')
+    # Information from the currently playing song are sorted and assigned
     track_id = results['item']['id']
     track_name = results['item']['name']
     artists = [artist for artist in results['item']['artists']]
@@ -31,16 +34,20 @@ def get_current_song():
 def main():
     current_track_id = None
     current_track = None
+    # Application runs in a infinite loop
     while True:
         try:
             prevTrack = current_track
             current_track = get_current_song()
+            # Track is only displayed if it is different from the last loop
             if current_track["id"] != current_track_id:
                 print(current_track)
                 current_track_id = current_track['id']
+                # test.jpg is updated with the album cover of the currently playing song
                 with open("test.jpg",'wb') as f:
                     f.write(requests.get(current_track['image']).content)
         except:
+            # If there is an exception, then the program loops again
             pass
         time.sleep(2)
 
